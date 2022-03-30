27d4e774673b1966bead885f90399a8f29df9dbb
daniel.silveira <daniel.repres@hotmail.com>
2022-03-29 21:06:42 -0300

    Alterando

--- detalhes/detalhes.js
+++ detalhes/detalhes.js
@@ -10,14 +10,16 @@ let lat = 0;
 let long = 0;
 let motor;
 
-if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(function (posicao) {
-        lat = posicao.coords.latitude;
-        long = posicao.coords.longitude;
-        initMap();
-    }, function (error) {
-        console.log(error)
-    })
+function obterLocalizacao() {
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(function (posicao) {
+            lat = posicao.coords.latitude;
+            long = posicao.coords.longitude;
+            initMap();
+        }, function (error) {
+            console.log(error)
+        })
+    }
 }
 
 fetch(`https://chocode.herokuapp.com/pedido/${pedidoId}`,
@@ -51,17 +53,82 @@ fetch(`https://chocode.herokuapp.com/pedido/${pedidoId}`,
         });
     })
     .catch(function (error) {
-        console.log('Erro: ' + error.message);
+        console.log('Erro: ' + error);
     });
 
-let posEntregador = { lat: lat, lng: long };
+let posEntregador = 'R. Projetada, 75 - Jardim Mercedes, Ferraz de Vasconcelos - SP, 08541-215';
 let posCliente = { lat: -23.5446941, lng: -46.3786544 };
+
+btnConcluir.addEventListener('click', event => {
+    clearInterval(motor);
+    pedidoEntregue();
+    proximaPagina();
+});
+
+btnCancelar.addEventListener('click', event => {
+    clearInterval(motor);
+    cancelarPedido();
+    proximaPagina();
+});
+
+
+function enviarLocalizacao() {
+    obterLocalizacao();
+    const dados = {
+        latitude: lat,
+        longitude: long,
+        idEntregador: entregadorId,
+        idPedido: pedidoId,
+    }
+    fetch("https://chocode.herokuapp.com/geolocalizacao",
+        {
+            method: "POST",
+            headers: {
+                "Authorization": token,
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(dados)
+        }).then(response => console.log(response))
+}
+
+function contarSegundos() {
+    motor = setInterval(() => {
+        initMap();
+        enviarLocalizacao();
+    }, 60000);
+}
+
+function cancelarPedido() {
+    fetch(`https://chocode.herokuapp.com/pedido/${pedidoId}/entregador/${entregadorId}/cancelado`,
+        {
+            method: "PUT",
+            headers: {
+                "Authorization": token
+            }
+        }).then(response => console.log(response))
+}
+
+function pedidoEntregue() {
+    fetch(`https://chocode.herokuapp.com/pedido/${pedidoId}/entregador/${entregadorId}/entregue`,
+        {
+            method: "PUT",
+            headers: {
+                "Authorization": token
+            }
+        }).then(response => console.log(response))
+}
+
+function proximaPagina() {
+    window.location.href = "https://chocode-ifood.github.io/front/pedidos/pedidos.html";
+}
 
 async function initMap() {
     const directionsService = new google.maps.DirectionsService();
     const directionsRenderer = new google.maps.DirectionsRenderer();
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 10,
+        center: { lat: lat, lng: long },
         styles: [
             {
                 "featureType": "administrative",
@@ -103,66 +170,6 @@ async function initMap() {
     });
 }
 
-btnConcluir.addEventListener('click', event => {
-    clearInterval(motor);
-    pedidoEntregue();
-    proximaPagina();
-});
-
-btnCancelar.addEventListener('click', event => {
-    clearInterval(motor);
-    cancelarPedido();
-    proximaPagina();
-});
-
-async function enviarLocalizacao() {
-    const dados = {
-        latitude: lat,
-        longitude: long,
-        idEntregador: entregadorId,
-        idPedido: pedidoId
-    }
-    fetch("https://chocode.herokuapp.com/geolocalizacao",
-        {
-            method: "POST",
-            headers: {
-                "Authorization": token,
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(dados)
-        }).then(response => console.log(response))
-}
-
-function contarSegundos() {
-    motor = setInterval(() => {
-        enviarLocalizacao();
-    }, 60000);
-}
-
-function cancelarPedido() {
-    fetch(`https://chocode.herokuapp.com/pedido/${pedidoId}/entregador/${entregadorId}/cancelado`,
-        {
-            method: "PUT",
-            headers: {
-                "Authorization": token
-            }
-        }).then(response => console.log(response))
-}
-
-function pedidoEntregue() {
-    fetch(`https://chocode.herokuapp.com/pedido/${pedidoId}/entregador/${entregadorId}/entregue`,
-        {
-            method: "PUT",
-            headers: {
-                "Authorization": token
-            }
-        }).then(response => console.log(response))
-}
-
-function proximaPagina() {
-    window.location.href = "https://chocode-ifood.github.io/front/pedidos/pedidos.html";
-}
 contarSegundos();
 enviarLocalizacao();
 
